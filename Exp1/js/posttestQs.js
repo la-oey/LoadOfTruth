a0635d29f9276aa6eb9f27d67150da451631d220
Lauren Oey <loey@ucsd.edu>
2022-01-25 23:13:54 -0800

    added several more post expt questions

--- Exp1/js/posttestQs.js
+++ Exp1/js/posttestQs.js
@@ -5,9 +5,39 @@ var postQs = [
 		"type": "free",
 		"answers": ""
 	},{
+		"id": "technical",
+		"question": "Did you have any technical difficulties?",
+		"type": "free",
+		"answers": ""
+	},{
+		"id": "attentive",
+		"question": "How attentive were you to the game?",
+		"type": "slider",
+		"answers": ["inattentive", "highly attentive"]
+	},{
+		"id": "checks",
+		"question": "How many memory checks do you think you got right?",
+		"type": "slider",
+		"answers": ["none of them", "all of them"]
+	},{
+		"id": "rolls",
+		"question": "How much were you paying attention to your roll?",
+		"type": "slider",
+		"answers": ["not at all", "all the time"]
+	},{
 		"id": "influence",
-		"question": "How much did knowing what you rolled influence what you told the other player?",
+		"question": "How much did you think your roll influence what you told the other player?",
 		"type": "slider",
 		"answers": ["not at all", "highly influential"]
+	},{
+		"id": "lie",
+		"question": "How often did you lie to the other player?",
+		"type": "slider",
+		"answers": ["never", "all the time"]
+	},{
+		"id": "ToM",
+		"question": "How often were you thinking about your strategy in terms of what the other player thought?",
+		"type": "slider",
+		"answers": ["never", "all the time"]
 	}
 ]
