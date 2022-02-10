7f41889a80ff9034ec57740c58f2d7b4760c0ee9
Lauren Oey <loey@ucsd.edu>
2022-02-09 17:09:22 -0800

    script to parse post test question responses

--- Exp1/jsonToCSV.py
+++ Exp1/jsonToCSV.py
@@ -8,6 +8,8 @@ mypath = 'data/' #set path to folder containing json files
 files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 raw = open('raw.csv','w')
 csvwriter = csv.writer(raw)
+posttest = open('posttest.csv','w')
+csvwriter2 = csv.writer(posttest)
 
 head = 0
 
@@ -18,16 +20,25 @@ for f in files: #iterate through files in folder
 			parsed = json.loads(content)
 			subjID = parsed["client"]["sid"]
 			subjData = parsed["trials"]
+			subjPost = parsed["client"]["posttest"]
 
 			if head == 0:
 				header = ["subjID"] #init header array
 				header.extend(subjData[0].keys())
 				csvwriter.writerow(header)
+				header2 = ["subjID","condition"]
+				header2.extend(subjPost.keys())
+				csvwriter2.writerow(header2)
 				head = 1
 
 			for s in subjData:
 				vals = [subjID] #init data array
 				vals.extend(s.values())
 				csvwriter.writerow(vals)
+
+			vals2 = [subjID, parsed["expt"]["catchQuestion"]]
+			vals2.extend(subjPost.values())
+			csvwriter2.writerow(vals2)
 raw.close()
+posttest.close()
 
